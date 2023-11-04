c9230d76b51d8220962aedff818b708e9fdebe3a
ali.davudov <davudovali@gmail.com>
2023-11-04 18:28:37 +0100

    Update routes

--- src/App.tsx
+++ src/App.tsx
@@ -8,12 +8,9 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/personal-site/resume" element={<Resume />} />
-                <Route
-                    path="/personal-site/statistic"
-                    element={<Statistics />}
-                />
-                <Route path="/personal-site" element={<WordsRepitor />} />
+                <Route path="/resume" element={<Resume />} />
+                <Route path="/statistic" element={<Statistics />} />
+                <Route path="/german" element={<WordsRepitor />} />
             </Routes>
         </BrowserRouter>
     )
